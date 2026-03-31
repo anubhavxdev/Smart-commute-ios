bf4569bc42cb27d05419b95ac7f8b53b5f264252
Anubhav Jaiswal <anubhav.jaiswal23@lpu.in>
2026-03-31 20:40:35 +0530

    Initial Commit

---
+++ SmartCommute/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartCommute
+//
+//  Created by piyush shah on 31/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartCommute/SmartCommuteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartCommuteApp.swift
+//  SmartCommute
+//
+//  Created by piyush shah on 31/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SmartCommuteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
